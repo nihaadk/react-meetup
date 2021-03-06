bdf8bd502046d254160aa0aa0ce5b333631e3d0a
Nihad Keric <nihadkeric@gmail.com>
2021-03-06 22:54:38 +0100

    use fetch function and fetch the data from firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,27 @@
+import { useHistory } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
-	return <div>New Meetup Page</div>;
+	const history = useHistory();
+
+	function addMeetupHandler(meetupData) {
+		fetch('https://react-course-5bdf2-default-rtdb.firebaseio.com/meetups.json', {
+			method: 'POST',
+			body: JSON.stringify(meetupData),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(() => {
+			history.replace('/');
+		});
+	}
+
+	return (
+		<section>
+			<h1>Add New Meetup</h1>
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
+		</section>
+	);
 }
 
 export default NewMeetupPage;
