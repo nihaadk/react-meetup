d2f136647c107c73e6e4f117954a796635188750
Nihad Keric <nihadkeric@gmail.com>
2021-03-07 14:34:55 +0100

    create favorites page with react context

---
+++ src/store/favorites-context.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from 'react';
+
+const FavoritesContext = createContext({
+	favorites: [],
+	totalFavorites: 0,
+	addFavorite: (favoriteMeetup) => {},
+	removeFavorite: (meetupId) => {},
+	itemIsFavorite: (meetupId) => {}
+});
+
+export function FavoritesContextProvider(props) {
+	const [ userFavorites, setUserFavorites ] = useState([]);
+
+	function addFavoriteHandler(favoriteMeetup) {
+		setUserFavorites((prevUseFavorites) => {
+			return prevUseFavorites.concat(favoriteMeetup);
+		});
+	}
+
+	function removeFavoriteHandler(meetupId) {
+		setUserFavorites((prevUseFavorites) => {
+			return prevUseFavorites.filter((meetup) => meetup.id !== meetupId);
+		});
+	}
+
+	function itemIsFavoriteHandler(meetupId) {
+		return userFavorites.some((meetup) => meetup.id === meetupId);
+	}
+
+	const context = {
+		favorites: userFavorites,
+		totalFavorites: userFavorites.length,
+		addFavorite: addFavoriteHandler,
+		removeFavorite: removeFavoriteHandler,
+		itemIsFavorite: itemIsFavoriteHandler
+	};
+
+	return <FavoritesContext.Provider value={context}>{props.children}</FavoritesContext.Provider>;
+}
+
+export default FavoritesContext;
